576fad9523d8e1b1ed9bb67e9428889b34681894
bakhtiaryus <yusufbakhtiar98@gmail.com>
2017-02-02 14:07:25 +0700

    geometricalculator

--- GemoteriCalculator/app/src/main/java/bakhtiaryus/gemotericalculator/MainActivity.java
+++ GemoteriCalculator/app/src/main/java/bakhtiaryus/gemotericalculator/MainActivity.java
@@ -33,26 +33,47 @@ public class MainActivity extends AppCompatActivity  implements  AdapterView.OnI
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         TextView text1 = (TextView) findViewById(R.id.txt1);
         TextView text2 = (TextView) findViewById(R.id.txt2);
-        EditText input2= (EditText) findViewById(R.id.input2);
-        
+        TextView text3 = (TextView) findViewById(R.id.txt3);
+        EditText input2 = (EditText) findViewById(R.id.input2);
+        EditText input3 = (EditText) findViewById(R.id.input3);
         String operator = parent.getItemAtPosition(position).toString();
-        if(operator.equalsIgnoreCase("Lingkaran"))
+        if (operator.equalsIgnoreCase("Lingkaran")) {
+            text1.setText("Jari-Jari");
+            input2.setEnabled(false);
+            input2.setEnabled(false);
+
+            input3.setEnabled(false);
+
+            text2.setText("");
+            text3.setText("");
+        } else if (operator.equalsIgnoreCase("segitiga")) {
+            text1.setText("Alas");
+            input2.setEnabled(true);
+            input3.setEnabled(false);
+            text2.setText("Tinggi");
+            text3.setText("");
+        } else if (operator.equalsIgnoreCase("Persegi panjang")) {
+            text1.setText("Panjang");
+            input2.setEnabled(true);
+            input3.setEnabled(false);
+            text2.setText("Lebar");
+            text3.setText("");
+        } else if (operator.equalsIgnoreCase("Balok")) {
+            text1.setText("Panjang");
+
+            text2.setText("Lebar");
+            input2.setEnabled(true);
+            text3.setText("Tinggi");
+        } else
         {
             text1.setText("Jari-Jari");
             input2.setEnabled(false);
-            text2.setText("");
-        }
-        else if(operator.equalsIgnoreCase("segitiga"))
-        {
-            text1.setText("Alas");
-            input2.setEnabled(true);
-            text2.setText("Tinggi");
 
-        }else {
-            text1.setText("Panjang");
-            input2.setEnabled(true);
-            text2.setText("Lebar");
+            input3.setEnabled(false);
+            text2.setText("");
+            text3.setText("");
         }
+
     }
 
     @Override
@@ -62,16 +83,19 @@ public class MainActivity extends AppCompatActivity  implements  AdapterView.OnI
     public void calculate(View view){
         EditText input1 = (EditText) findViewById(R.id.input1);
         EditText input2 = (EditText) findViewById(R.id.input2);
+        EditText input3 = (EditText) findViewById(R.id.input3);
         Spinner spinner = (Spinner) findViewById(R.id.geometri);
-        double inp1 = 0 ,inp2 = 0;
+        double inp1 = 0 ,inp2 = 0 ,inp3 = 0;
 
         String operator = spinner.getSelectedItem().toString();
         String hasil = "";
          inp1 = Double.parseDouble(input1.getText().toString());
+
         if(input2.isEnabled())
         {
             inp2 = Double.parseDouble(input2.getText().toString());
         }
+        inp3 = Double.parseDouble(input3.getText().toString());
 
 
         if(operator.equalsIgnoreCase("Lingkaran")){
@@ -84,12 +108,22 @@ public class MainActivity extends AppCompatActivity  implements  AdapterView.OnI
             double phy=Math.sqrt((inp1*inp1)+(inp2*inp2)/2);
             hasil += "Keliling dari Segitiga adalah : "+(inp1+inp2+phy)+"\n";
         }
-        else
+        else if (operator.equalsIgnoreCase("Persegi"))
         {
             hasil = "luas dari Persegi panjang adalah adalah : "+(inp1*inp2)+"\n";
             hasil += "Keliling dari persegi panjang adalah : "+(2*(inp1+inp2))+"\n";
         }
-        TextView result=(TextView) findViewById(R.id.result);
+        else if (operator.equalsIgnoreCase("Balok"))
+        {
+            hasil = "luas dari Balok adalah adalah : "+(2 *((inp1 * inp2)+(inp1 * inp3)+(inp2 * inp3)))+"\n";
+            hasil += "volume dari Balok adalah : "+(inp1*inp2*inp3)+"\n";
+        }
+        else
+        {
+            hasil = "Luas permukaannya = "+((3.14*inp1*inp1)*4)+"\n";
+            hasil += "Volumenya = "+(((3.14*inp1*inp1*inp1)*4)/3)+"\n";
+        }
+            TextView result=(TextView) findViewById(R.id.result);
         result.setText(hasil);
 
     }
